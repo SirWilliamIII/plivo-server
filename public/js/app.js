b0ac0d7a79c25ef1dffd2a211f30b75005afde47
SirWilliamIII <willcarpenterwastaken@gmail.com>
2020-04-27 09:05:07 -0500

    changed regex on phone number validation to allow for + sign

--- public/js/app.js
+++ public/js/app.js
@@ -24,7 +24,7 @@ const resultMessage = document.querySelector('#resultsMessage1')
 const resultMessageThree = document.querySelector('#resultsMessage3')
 
 const isValidNum = (num) => {
-    var valNum = /^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$/;
+    var valNum = /^\+?\d*$/;
     if (num.value.match(valNum)) {
         return true;
     } else {
